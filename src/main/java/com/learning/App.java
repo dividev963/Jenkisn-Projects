d871e1ee764f4497f47131e02e9d3bcee1895625
Divi Dev <divyadevnaidu123@gmail.com>
2021-03-08 21:57:44 +0530

    first demo maven project

---
+++ src/main/java/com/learning/App.java
@@ -0,0 +1,19 @@
+package com.learning;
+
+public class App {
+ public String Test()
+ {
+	 return "test";
+ }
+ 
+ public void display()
+ {
+	 System.out.print("Hello World!!");
+ }
+ 
+ public static void main()
+ {
+	 App app=new App();
+	 app.display();
+ }
+}
---
+++ src/test/java/com/learning/AppTest.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+	Assert.assertTrue(true);
+	}
+
+}
